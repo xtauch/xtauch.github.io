31cc7700f40ce517ebf183b0a0c981be72a2a5c1
Xavier TAUCH <xavier.tauch@gmail.com>
2020-07-05 09:40:21 +0200

    Delete contact.js

--- P5/js/contact.js
+++
@@ -1,9 +0,0 @@
-class Contact {
-    constructor(firstName, lastName, address, city, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.email = email;
-    }
-}
