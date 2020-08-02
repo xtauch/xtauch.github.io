06ab47ce6006da2e35bf908f98aa5a48cfbcaa5e
Xavier TAUCH <xavier.tauch@gmail.com>
2020-08-02 08:53:01 +0200

    Add files via upload

---
+++ P6/backend/models/Sauce.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const sauceSchema = mongoose.Schema({
+    _id: {type: String, required: false},
+    name: {type: String, required: true},
+    manufacturer: {type: String, required: true},
+    description: {type: String, required: true},
+    heat: {type: Number, required: true},
+    likes: {type: Number, required: false},
+    dislikes: {type: Number, required: false},
+    imageUrl: {type: String, required: true},
+    mainPepper: {type: String, required: true},
+    usersLiked: {type: String, required: false},
+    usersDisliked: {type: String, required: false},
+    userId: {type: String, required: false},
+})
+
+module.exports = mongoose.model('Sauce', sauceSchema)
