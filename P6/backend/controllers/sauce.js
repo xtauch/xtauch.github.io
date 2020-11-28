7c3da5cedf2d14bdb23993067d38120b33b08985
Xavier TAUCH <xavier.tauch@gmail.com>
2020-11-28 14:12:41 +0100

    Update sauce.js

--- P6/backend/controllers/sauce.js
+++ P6/backend/controllers/sauce.js
@@ -55,24 +55,24 @@ exports.getAllSauce = (req, res, next) => {
 
 
 exports.likeSauce = (req, res, next) => {
-    if (req.body.like === 1) { // si l'utilisateur aime la sauce
-        Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId } }) // on ajoute 1 like et on le push l'array usersLiked
-            .then((sauce) => res.status(200).json({ message: 'Un like de plus !' }))
+    if (req.body.like === 1) {
+        Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId } })
+            .then((sauce) => res.status(200).json({ message: '+1 like' }))
             .catch(error => res.status(400).json({ error }));
-    } else if (req.body.like === -1) { // si il aime pas la sauce
-        Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: (req.body.like++) * -1 }, $push: { usersDisliked: req.body.userId } }) // on ajoute 1 dislike et on le push l'array usersDisliked
-            .then((sauce) => res.status(200).json({ message: 'Un dislike de plus !' }))
+    } else if (req.body.like === -1) {
+        Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: (req.body.like++) * -1 }, $push: { usersDisliked: req.body.userId } })
+            .then((sauce) => res.status(200).json({ message: '+1 dislike' }))
             .catch(error => res.status(400).json({ error }));
-    } else { // si l'utilisateur enleve son vote, like === 0
+    } else { 
         Sauce.findOne({ _id: req.params.id })
             .then(sauce => {
-                if (sauce.usersLiked.includes(req.body.userId)) { // si l'array userLiked contient le id de like
-                    Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }) // $pull : ça vide l'array userLiked et ça enleve un like sinon le meme utilisateur pourrai ajouter plusieurs like
-                        .then((sauce) => { res.status(200).json({ message: 'Un like de moins !' }) })
+                if (sauce.usersLiked.includes(req.body.userId)) {
+                    Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
+                        .then((sauce) => { res.status(200).json({ message: '-1 like' }) })
                         .catch(error => res.status(400).json({ error }))
                 } else if (sauce.usersDisliked.includes(req.body.userId)) {
                     Sauce.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
-                        .then((sauce) => { res.status(200).json({ message: 'Un dislike de moins !' }) })
+                        .then((sauce) => { res.status(200).json({ message: '-1 dislike' }) })
                         .catch(error => res.status(400).json({ error }))
                 }
             })
